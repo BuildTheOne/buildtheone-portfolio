0794d6e73378015302ee5e8b0360ccc36ef4794c
Ignatius Henriyanto Primai Renda <76039582+BuildTheOne@users.noreply.github.com>
2023-06-06 13:07:04 +0700

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BuildTheOne</title>
+      </Head>
+      
+      <main className='flex'>
+
+      </main>
+    </>
+  )
+}
